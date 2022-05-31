cbb0c491658ba80bd3d16666fe5189804a518c34
andru <andru2400@hotmail.com>
2022-05-31 17:43:42 -0500

    add seeder field_option

--- database/seeders/FieldOptionSeeder.php
+++ database/seeders/FieldOptionSeeder.php
@@ -13,6 +13,9 @@ class FieldOptionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('field_option')->insert([
+            ["id"=>1, "field_id"=>4, "option_id"=>1],
+            ["id"=>2, "field_id"=>4, "option_id"=>2]
+        ]);
     }
 }
