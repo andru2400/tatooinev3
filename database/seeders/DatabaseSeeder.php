fdee1fb88804e506019032a26c427d7e5f5cb846
andru <andru2400@hotmail.com>
2022-05-30 21:12:38 -0500

    add seeders default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // $this->call();
+
+        $this->call(AdminUsersSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(RolesSeeder::class);
+        $this->call(ModelHasRolesSeeder::class);
+        $this->call(PermissionsSeeder::class);
+        $this->call(RoleHasPermissionsSeeder::class);
+        $this->call(TranslationsSeeder::class);
+        $this->call(CampaignsSeeder::class);
+        $this->call(FieldTypesSeeder::class);
+        $this->call(FieldsSeeder::class);
+        $this->call(CampaignFieldSeeder::class);
+        $this->call(RulesSeeder::class);
+        $this->call(CampaignFieldRuleSeeder::class);
+        $this->call(OptionsSeeder::class);
+        $this->call(FieldOptionSeeder::class);
+
     }
 }
