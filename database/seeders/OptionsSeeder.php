13200aea29a52a81581575208b3009d1230d4a4a
andru <andru2400@hotmail.com>
2022-05-31 17:38:44 -0500

    add seeder options

--- database/seeders/OptionsSeeder.php
+++ database/seeders/OptionsSeeder.php
@@ -13,6 +13,10 @@ class OptionsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('options')->insert([
+            ["id"=>1,"name"=>"Option1","value"=>"option_1","created_at"=>"2022-05-30 15:40:47","updated_at"=>"2022-05-30 15:40:47"],
+            ["id"=>2,"name"=>"Option2","value"=>"option_2","created_at"=>"2022-05-30 15:41:05","updated_at"=>"2022-05-30 15:41:05"],
+            ["id"=>3,"name"=>"Option3","value"=>"option_3","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"]
+        ]);
     }
 }
