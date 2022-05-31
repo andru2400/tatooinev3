1528d820484838578246eb4888c1d6fe95bc27c1
andru <andru2400@hotmail.com>
2022-05-30 21:43:21 -0500

    add seeder data Admin User

--- database/seeders/AdminUsersSeeder.php
+++ database/seeders/AdminUsersSeeder.php
@@ -13,6 +13,22 @@ class AdminUsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('admin_users')->insert([
+            [
+             'id'               => '1',
+             'first_name'       => 'Administrator',
+             'last_name'        => 'Administrator',
+             'email'            => 'administrator@bellpi.com',
+             'password'         => bcrypt('Admin@digital'),
+             'remember_token'   => null,
+             'activated'        => 1,
+             'forbidden'        => 0,
+             'language'         => 'en',
+             'deleted_at'       => null,
+             'created_at'       => '2022-05-29 20:35:35',
+             'updated_at'       => '2022-05-30 14:29:52',
+             'last_login_at'    => '2022-05-30 13:35:54'
+            ],
+       ]);
     }
 }
