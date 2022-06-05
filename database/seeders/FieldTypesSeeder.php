fd82b2668395dfd69a936f08caecb4647499d43d
andru <andru2400@hotmail.com>
2022-06-05 09:58:18 -0500

    add change seeder

--- database/seeders/FieldTypesSeeder.php
+++ database/seeders/FieldTypesSeeder.php
@@ -18,10 +18,11 @@ class FieldTypesSeeder extends Seeder
             ["id"=>1,"name"=>"text","description"=>"Tipo Texto","created_at"=>"2022-05-30 15:45:52","updated_at"=>"2022-05-30 15:45:52"],
             ["id"=>2,"name"=>"number","description"=>"Tipo Numero","created_at"=>"2022-05-30 15:46:39","updated_at"=>"2022-05-30 15:46:39"],
             ["id"=>3,"name"=>"email","description"=>"Tipo Email","created_at"=>"2022-05-30 15:47:01","updated_at"=>"2022-05-30 15:47:01"],
-            ["id"=>4,"name"=>"radio","description"=>"Tipo Radio","created_at"=>"2022-05-30 15:47:31","updated_at"=>"2022-05-30 15:47:31"],
-            ["id"=>5,"name"=>"checkbox","description"=>"Tipo checkbox","created_at"=>"2022-05-30 15:47:53","updated_at"=>"2022-05-30 15:47:53"],
-            ["id"=>6,"name"=>"select","description"=>"Es una etiqueta, no input, tipo seleccione","created_at"=>"2022-05-30 15:49:13","updated_at"=>"2022-05-30 15:51:28"],
-            ["id"=>7,"name"=>"textarea","description"=>"Es una etiqueta, no input, tipo área de texto","created_at"=>"2022-05-30 15:50:37","updated_at"=>"2022-05-30 15:50:37"],
+            ["id"=>4,"name"=>"date","description"=>"Tipo Fecha","created_at"=>"2022-05-30 15:47:31","updated_at"=>"2022-05-30 15:47:31"],
+            ["id"=>5,"name"=>"datetime-local","description"=>"Tipo Fecha Hora","created_at"=>"2022-05-30 15:47:53","updated_at"=>"2022-05-30 15:47:53"],
+            ["id"=>6,"name"=>"time","description"=>"Tipo Hora","created_at"=>"2022-05-30 15:47:53","updated_at"=>"2022-05-30 15:47:53"],
+            ["id"=>7,"name"=>"select","description"=>"Tipo Seleccion (opciones)","created_at"=>"2022-05-30 15:49:13","updated_at"=>"2022-05-30 15:51:28"],
+            ["id"=>8,"name"=>"textarea","description"=>"Tipo Texto largo","created_at"=>"2022-05-30 15:50:37","updated_at"=>"2022-05-30 15:50:37"],
         ]);
 
     }
--- database/seeders/FieldsSeeder.php
+++ database/seeders/FieldsSeeder.php
@@ -18,7 +18,7 @@ class FieldsSeeder extends Seeder
             ["id"=>1,"name"=>"full_name","field_type_id"=>1,"description"=>"Nombre Completo","created_at"=>"2022-05-30 15:54:05","updated_at"=>"2022-05-30 15:54:05"],
             ["id"=>2,"name"=>"phone","field_type_id"=>2,"description"=>"Numero de telefono","created_at"=>"2022-05-30 15:55:31","updated_at"=>"2022-05-30 15:55:31"],
             ["id"=>3,"name"=>"email","field_type_id"=>3,"description"=>"Correo Electronico","created_at"=>"2022-05-30 15:56:24","updated_at"=>"2022-05-30 15:56:24"],
-            ["id"=>4,"name"=>"document_types","field_type_id"=>6,"description"=>"Tipos de documento","created_at"=>"2022-05-30 16:29:47","updated_at"=>"2022-05-30 16:29:47"]
+            ["id"=>4,"name"=>"document_types","field_type_id"=>7,"description"=>"Tipos de documento","created_at"=>"2022-05-30 16:29:47","updated_at"=>"2022-05-30 16:29:47"]
         ]);
     }
 }
