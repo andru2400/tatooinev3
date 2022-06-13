88f5f1c6637543dcad1781331c7d12e9401c14fc
andru <andru2400@hotmail.com>
2022-06-13 18:28:24 -0500

    add seeder optionseeder

--- database/seeders/FieldOptionSeeder.php
+++ database/seeders/FieldOptionSeeder.php
@@ -15,8 +15,32 @@ class FieldOptionSeeder extends Seeder
     public function run()
     {
         DB::table('field_option')->insert([
-            ["id"=>1, "field_id"=>4, "option_id"=>1],
-            ["id"=>2, "field_id"=>4, "option_id"=>2]
+            ["id"=>1, "field_id"=>12, "option_id"=>1],
+            ["id"=>2, "field_id"=>12, "option_id"=>2],
+            ["id"=>3, "field_id"=>12, "option_id"=>3],
+            ["id"=>4, "field_id"=>12, "option_id"=>4],
+            ["id"=>5, "field_id"=>12, "option_id"=>5],
+            ["id"=>6, "field_id"=>12, "option_id"=>6],
+            ["id"=>7, "field_id"=>12, "option_id"=>7],
+            ["id"=>8, "field_id"=>12, "option_id"=>8],
+            ["id"=>9, "field_id"=>12, "option_id"=>9],
+            ["id"=>10, "field_id"=>12, "option_id"=>10],
+            ["id"=>11, "field_id"=>12, "option_id"=>11],
+            ["id"=>12, "field_id"=>12, "option_id"=>12],
+
+            ["id"=>13, "field_id"=>15, "option_id"=>13],
+            ["id"=>14, "field_id"=>15, "option_id"=>14],
+            ["id"=>15, "field_id"=>15, "option_id"=>15],
+            ["id"=>16, "field_id"=>15, "option_id"=>16],
+
+            ["id"=>17, "field_id"=>26, "option_id"=>17],
+            ["id"=>18, "field_id"=>26, "option_id"=>18],
+            ["id"=>19, "field_id"=>26, "option_id"=>19],
+            ["id"=>20, "field_id"=>26, "option_id"=>20],
+
+            ["id"=>21, "field_id"=>28, "option_id"=>21],
+            ["id"=>22, "field_id"=>28, "option_id"=>22],
+
         ]);
     }
 }
--- database/seeders/OptionsSeeder.php
+++ database/seeders/OptionsSeeder.php
@@ -15,9 +15,31 @@ class OptionsSeeder extends Seeder
     public function run()
     {
         DB::table('options')->insert([
-            ["id"=>1,"name"=>"Option1","value"=>"option_1","created_at"=>"2022-05-30 15:40:47","updated_at"=>"2022-05-30 15:40:47"],
-            ["id"=>2,"name"=>"Option2","value"=>"option_2","created_at"=>"2022-05-30 15:41:05","updated_at"=>"2022-05-30 15:41:05"],
-            ["id"=>3,"name"=>"Option3","value"=>"option_3","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"]
+            ["id"=>1,"name"=>"Audi","value"=>"audi","created_at"=>"2022-05-30 15:40:47","updated_at"=>"2022-05-30 15:40:47"],
+            ["id"=>2,"name"=>"Chevrolet","value"=>"chevrolet","created_at"=>"2022-05-30 15:41:05","updated_at"=>"2022-05-30 15:41:05"],
+            ["id"=>3,"name"=>"Ford","value"=>"ford","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>4,"name"=>"Kia","value"=>"kia","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>5,"name"=>"Mazda","value"=>"mazda","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>6,"name"=>"Mercedes Benz","value"=>"mercedes_benz","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>7,"name"=>"Mitsubishi","value"=>"mitsubishi","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>8,"name"=>"Peugeot","value"=>"peugeot","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>9,"name"=>"Renault","value"=>"renault","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>10,"name"=>"Suzuki","value"=>"suzuki","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>11,"name"=>"Toyota","value"=>"toyota","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>12,"name"=>"Volkswagen","value"=>"volkswagen","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+
+            ["id"=>13,"name"=>"Cédula de ciudadanía","value"=>"citizenship_card","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>14,"name"=>"Cédula de extranjería","value"=>"foreigner_id","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>15,"name"=>"Pasaporte","value"=>"passport","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>16,"name"=>"NIT","value"=>"nit","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+
+            ["id"=>17,"name"=>"Queja","value"=>"complain","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>18,"name"=>"Reclamo","value"=>"claim","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>19,"name"=>"Pregunta","value"=>"ask","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>20,"name"=>"Otro","value"=>"other","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+
+            ["id"=>21,"name"=>"Otro","value"=>"other","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
+            ["id"=>22,"name"=>"Otro","value"=>"other","created_at"=>"2022-05-30 15:41:25","updated_at"=>"2022-05-30 15:41:25"],
         ]);
     }
 }
