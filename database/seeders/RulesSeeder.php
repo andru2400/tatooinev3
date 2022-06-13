f3cce34ab6cc5308e0685b98970f27264ef3cb1b
andru <andru2400@hotmail.com>
2022-06-13 11:42:50 -0500

    Add seeder Rule

--- database/seeders/RulesSeeder.php
+++ database/seeders/RulesSeeder.php
@@ -15,10 +15,17 @@ class RulesSeeder extends Seeder
     public function run()
     {
         DB::table('rules')->insert([
-            ["id"=>1,"name"=>"required","validation_string"=>"Debe ser requerido","created_at"=>"2022-05-30 16:04:50","updated_at"=>"2022-05-30 16:04:50"],
-            ["id"=>2,"name"=>"integer","validation_string"=>"Debe ser numérico","created_at"=>"2022-05-30 16:06:11","updated_at"=>"2022-05-30 16:06:11"],
-            ["id"=>3,"name"=>"email","validation_string"=>"Debe ser correo electrónico","created_at"=>"2022-05-30 16:11:20","updated_at"=>"2022-05-30 16:11:20"],
-            ["id"=>4,"name"=>"max","validation_string"=>"Debe tener máximo de caracteres","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"]
+            ["id"=>1,"name"=>"Requerido",               "validation_string"=>"required","created_at"=>"2022-05-30 16:04:50","updated_at"=>"2022-05-30 16:04:50"],
+            ["id"=>2,"name"=>"Texto",                   "validation_string"=>"string","created_at"=>"2022-05-30 16:06:11","updated_at"=>"2022-05-30 16:06:11"],
+            ["id"=>3,"name"=>"Email",                   "validation_string"=>"email","created_at"=>"2022-05-30 16:11:20","updated_at"=>"2022-05-30 16:11:20"],
+            ["id"=>4,"name"=>"Fecha",                   "validation_string"=>"date","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>5,"name"=>"Numérico",                "validation_string"=>"numeric","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>6,"name"=>"Boleano(true, false)",    "validation_string"=>"numeric","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>7,"name"=>"Alfa",                    "validation_string"=>"alfa","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>8,"name"=>"Alfa Numérico",           "validation_string"=>"alfa_num","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>9,"name"=>"Mínimo de caracteres",    "validation_string"=>"min:","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>10,"name"=>"Máximo de caracteres",   "validation_string"=>"max:","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"],
+            ["id"=>11,"name"=>"Regex Expresion regular","validation_string"=>"regex:","created_at"=>"2022-05-30 16:14:00","updated_at"=>"2022-05-30 16:14:00"]
         ]);
     }
 }
